7dae406165bc435d9e78c310e1a12dfffbc71be6
rodrigogawa <56047207+rodrigogawa@users.noreply.github.com>
2021-04-09 21:57:26 -0300

    Updated lambda function for pessoal_ativo

--- etl/extracao/lambda-function/pessoal_ativo/lambda_function.py
+++ etl/extracao/lambda-function/pessoal_ativo/lambda_function.py
@@ -1,6 +1,6 @@
 from decouple import config
 from datetime import datetime
-from pessoal_ativo_api import pa_api
+from pessoal_ativo_api import pessoal_ativo_api
 import json
 import boto3
 
@@ -9,6 +9,9 @@ BUCKET = config('BUCKET')
 AWS_REGION = config('AWS_REGION')
 BUCKET_AWS_ACCESS_KEY_ID = config('BUCKET_AWS_ACCESS_KEY_ID')
 BUCKET_AWS_SECRET_ACCESS_KEY = config('BUCKET_AWS_SECRET_ACCESS_KEY')
+CUSTO_STN_SOURCES_STATES = 'custos_stn_sources_states.json'
+CUSTO_STN_PESSOAL_ATIVO_PATH = 'pessoal_ativo'
+custos_stn_state = {}
 
 S3 = boto3.resource(
     's3',
@@ -17,39 +20,101 @@ S3 = boto3.resource(
     aws_secret_access_key = BUCKET_AWS_SECRET_ACCESS_KEY
 )
 
-print('S3 Resource created')
+def load_s3_file_content(s3_path, s3_file_content):
+    # Initializes the bucket object
+    bucket = S3.Bucket(BUCKET)
+    # Write to file
+    bucket.put_object(Key=s3_path, Body=s3_file_content)
+
+def is_s3_file_exists(s3_path):
+    """
+        Verifies if the file in the s3_path in the bucket exists
+    """
+    bucket = S3.Bucket(BUCKET)
+
+    if len(list(bucket.objects.filter(Prefix=s3_path))) > 0:
+        return True
+    
+    return False
+
+def get_s3_file_content(s3_path):
+    
+    if not is_s3_file_exists(s3_path):
+        if s3_path == CUSTO_STN_SOURCES_STATES:
+            content = get_initial_state(s3_path)
+            if content:
+                load_s3_file_content(s3_path, json.dumps(content,indent = 2) )
+            else:
+                raise ValueError('It was not able to initialize the state ')
+        else:
+            load_s3_file_content(s3_path, '')
+    
+    obj = S3.Object(BUCKET, s3_path)
+
+    # Get content
+    return obj.get()['Body'].read().decode('utf-8')
+
+
+def get_initial_state(s3_path):
+
+    custos_stn_sources_states_file = open('custos_stn_sources_states.json',)
+    if custos_stn_sources_states_file:
+        return json.load(custos_stn_sources_states_file)
+    
+    return None
+
+def consume_api(custos_stn_state,pessoal_ativo):
+    # carregar o arquivo de estado
+    pessoal_ativo_items = pessoal_ativo.get_items_from_api()
+
+    if len(pessoal_ativo_items['items']) > 0:
+
+        pessoal_ativo.initial_offset = pessoal_ativo_items['initial_offset']
+        pessoal_ativo.file_number = pessoal_ativo_items['file_number']
+        custos_stn_state['sources']['pessoal_ativo']['initial_offset'] = pessoal_ativo_items['initial_offset']
+        custos_stn_state['sources']['file_number'] = pessoal_ativo_items['file_number']
+        
+        load_s3_file_content(CUSTO_STN_SOURCES_STATES, json.dumps(custos_stn_state, indent= 2))
+        load_s3_file_content(f'{CUSTO_STN_PESSOAL_ATIVO_PATH}/pessoal_ativo_{pessoal_ativo.file_number}', json.dumps(pessoal_ativo_items['items'], indent= 2))
+
+    while pessoal_ativo_items['hasMore'] and pessoal_ativo_items['initial_offset'] <= 750:
+        
+        pessoal_ativo_items = pessoal_ativo.get_items_from_api()
+        
+        pessoal_ativo.initial_offset = pessoal_ativo_items['initial_offset']
+        pessoal_ativo.file_number = pessoal_ativo_items['file_number'] + 1
+        pessoal_ativo.is_full_load = False
+        custos_stn_state['sources']['pessoal_ativo']['initial_offset'] = pessoal_ativo.initial_offset
+        custos_stn_state['sources']['pessoal_ativo']['file_number'] =  pessoal_ativo.file_number
+        custos_stn_state['sources']['pessoal_ativo']['is_full_load'] = pessoal_ativo.is_full_load
+        load_s3_file_content(CUSTO_STN_SOURCES_STATES, json.dumps(custos_stn_state, indent= 2))
+        load_s3_file_content(f'{CUSTO_STN_PESSOAL_ATIVO_PATH}/pessoal_ativo_{pessoal_ativo.file_number}', json.dumps(pessoal_ativo_items['items'], indent= 2))
+        
+
+
+def reset_state(pessoal_ativo,custos_stn_state):
+    pessoal_ativo.initial_offset = 0
+    pessoal_ativo.file_number = 0
+    custos_stn_state['sources']['pessoal_ativo']['initial_offset'] = pessoal_ativo.initial_offset
+    custos_stn_state['sources']['file_number'] = pessoal_ativo.file_number
+    
+    load_s3_file_content(CUSTO_STN_SOURCES_STATES, json.dumps(custos_stn_state, indent= 2))
 
 def handle_data_with_s3():
+    custos_stn_state = json.loads(get_s3_file_content(CUSTO_STN_SOURCES_STATES))
+    pessoal_ativo = pessoal_ativo_api(custos_stn_state['sources']['pessoal_ativo'])
     
-    bucket = S3.Bucket(BUCKET)
-    obj = bucket.Object('custo-stn-sources.json')
-    data = obj.get()['Body'].read()
-    custo_stn_sources = json.loads(data)
-    print(custo_stn_sources)
-    
-    source_name = 'pessoal_ativo'
-    pessoal_ativo_state = custo_stn_sources['sources'][source_name]
-    
-    # write
-    # bucket = S3.Bucket(BUCKET)
-    # obj = bucket.Object('custo-stn-sources.json')
-    # data = obj.get()['Body'].read()
-    # custo_stn_sources = json.loads(data)
-    # pessoal_ativo = custo_stn_sources['sources']['pessoal_ativo']
-    # pessoal_ativo['initial_offset'] = 0
-    # obj.put(Body=json.dumps(custo_stn_sources))
-     
-    pessoal_ativo = pa_api()
-
-    pessoal_ativo.get_files_by_endpoint(pessoal_ativo_state, bucket, BUCKET)
+    if pessoal_ativo.is_full_load:
+        reset_state(pessoal_ativo,custos_stn_state)
+        consume_api(custos_stn_state,pessoal_ativo)
+    elif not pessoal_ativo.is_full_load:
+        consume_api(custos_stn_state,pessoal_ativo)
 
 def lambda_handler(event, context):
+    
     handle_data_with_s3()
     
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    return {"state": {}}
 
 if __name__ == '__main__':
     lambda_handler({"state": {}}, None)
---
+++ etl/extracao/lambda-function/pessoal_ativo/pessoal_ativo_api.py
@@ -0,0 +1,33 @@
+from time import sleep
+import json
+import requests
+
+class pessoal_ativo_api():
+
+    def __init__(self, pessoal_ativo):
+        self.endpoint_url = pessoal_ativo['endpoint_url']
+        self.initial_offset = pessoal_ativo['initial_offset']
+        self.is_full_load = bool(pessoal_ativo['is_full_load'])
+        self.incremental_offset = pessoal_ativo['incremental_offset']
+        self.file_number = pessoal_ativo['file_number']
+
+    def get_items_from_api(self):
+        parameters = {
+            "offset": self.initial_offset
+        }
+
+        response = requests.request("GET", self.endpoint_url, params=parameters).json()
+
+        all_items = {}
+        all_items['items'] = []
+        
+        for item in response['items']:
+            all_items['items'].append(item)
+          
+        all_items['file_number'] = self.file_number
+        
+        all_items['hasMore'] = response['hasMore']
+        all_items['initial_offset'] = self.initial_offset + self.incremental_offset
+
+        if len(all_items['items']) > 0:
+            return all_items
