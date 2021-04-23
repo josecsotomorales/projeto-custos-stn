ab96a0afafc059ab6456eb6ed4e09e857e878935
rodrigogawa <56047207+rodrigogawa@users.noreply.github.com>
2021-04-22 22:44:20 -0300

    Atualizado docker para dbt
    Criado modelo dummy

--- docker/airflow/dags/dbt_test.py
+++ docker/airflow/dags/dbt_test.py
@@ -6,28 +6,29 @@ from airflow.operators.bash_operator import BashOperator
 
 from datetime import timedelta
 import datetime
+
 default_args = {
     'owner' : 'airflow',
     'retries' : 0
 }
 
 dag = DAG(
-    dag_id = 'dbt_dags_test_3',
+    dag_id = 'dbt_dags_test_4',
     start_date = airflow.utils.dates.days_ago(2),
-    default_args = default_args,
     schedule_interval = '* * * * *',
-    catchup=False
+    catchup=False,
+    default_args = default_args
 )
 
 dbt_run = BashOperator(
     task_id='dbt_debug',
-    bash_command='dbt debug',
+    bash_command= 'cd /usr/app/dbt;dbt debug',
     dag=dag
 )
 
 dbt_test = BashOperator(
     task_id='dbt_compile',
-    bash_command='dbt compile',
+    bash_command='cd /usr/app/dbt; dbt compile',
     dag=dag
 )
 
